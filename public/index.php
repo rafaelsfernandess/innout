0371cf974089f57885afccc74172ee4c58f22f16
rafaelsfernandess <rafaelsfernandes@outlook.com>
2022-09-16 16:57:12 -0300

    Criação do select

--- public/index.php
+++ public/index.php
@@ -1,11 +1,10 @@
 <?php
 
-require_once(dirname(__FILE__, 2).'/src/config/database.php');
+require_once(dirname(__FILE__, 2).'/src/config/config.php');
+require_once(dirname(__FILE__, 2).'/src/models/User.php');
 
-$sql = 'select * from users';
-$result = Database::getResultFromQuery($sql);
 
-while($row = $result->fetch_assoc()){
-    print_r($row);
-    echo '<br>';
-}
+$user = new User(['name'=>'Lucas', 'email'=>'lucas@lucas.com']);
+echo $user->getSelect();
+
+echo User::getSelect(['name'=>'Chaves'],'name, email');
---
+++ src/models/Model.php
@@ -0,0 +1,59 @@
+<?php
+
+class Model{
+
+    protected static $tableName= '';
+    protected static $columns= [];
+    protected $values= [];
+
+    function __construct($arr){
+        $this->loadFromArray($arr);
+    }
+
+    public function loadFromArray($arr){
+        if($arr){
+                foreach ($arr as $key => $value) {
+                    $this->$key = $value;
+            }
+        }
+    }
+
+    public function __get($key){
+        return $this->values[$key];
+    }
+
+    public function __set($key, $value){
+        $this->values[$key] = $value;
+    }
+
+    public static function getSelect($filters=[], $columns = '*'){
+        $sql =  "SELECT ${columns} FROM "
+                .static::$tableName
+                .static::getFilters($filters);
+        return $sql;
+    }
+
+    private static function getFilters($filters){
+        $sql='';
+        if(count($filters)>0){
+            $sql.=" WHERE 1=1";
+            foreach ($filters as $columns => $value) {
+                $sql.=" AND ${columns} = ". static::getFormatedValue($value);
+            }    
+        }
+        return $sql;
+    }
+
+    private static function getFormatedValue($value){
+        if(is_null($value)){
+            return "null";
+        }elseif(gettype($value)==='string'){
+            return "'${value}'";
+        }else {
+            return $value;
+        }
+    }
+
+}
+
+?>
